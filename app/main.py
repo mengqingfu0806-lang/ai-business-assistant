179b3613ec393bb63c4c293fd955b16bb9dd4975
mengqingfu0806-lang <mengqingfu0806@gmail.com>
2026-09-10 10:10:06 +0800

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,9 @@
 from config import settings
-
+from core.assistant import Assistant
 
 print(settings.PROJECT_NAME)
 print(settings.VERSION)
 print(settings.ENVIRONMENT)
+assistant = Assistant()
+
+print(assistant.initialize())
